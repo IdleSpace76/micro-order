d9e69f2d70d2b3671f2270687e352984419864b0
Жаров Андрей <rougik3000@gmail.com>
2025-08-26 13:59:21 +0300

    Initial commit

---
+++ src/main/java/ru/micro/customer/MicroCustomerApplication.java
@@ -0,0 +1,13 @@
+package ru.micro.customer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroCustomerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroCustomerApplication.class, args);
+	}
+
+}
